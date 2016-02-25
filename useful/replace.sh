62d9b9a3ddd50563666c55cb48c7790ac168fb93
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2016-02-26 00:54:13 +0100

    Some fixes for yaml files

--- useful/replace.sh
+++ useful/replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find ../ -name "parsers.yaml" -print0 | xargs -0 sed -i 's/---/---\nclass: RegexParser\nlog_type : default/g'
 find ../ -name "rules*.yaml" -print0 | xargs -0 sed -i 's/cause: \([a-z]*\)/causes: [\1]/g'
-echo -e "---\n['RegexParser', {'type': 'default', 'path_pattern': 'node_1.log'}]" > log_locations.yaml
+echo -e "---\n['RegexFilenameParser', {'type': 'default', 'path_pattern': 'node_1.log'}]" > log_locations.yaml
 for i in $( find ../whylog/ -name "[0-9]*" -type d); do
     cp log_locations.yaml $i
 done
